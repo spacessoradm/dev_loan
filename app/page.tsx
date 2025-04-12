af1d9103fd3175c4921f7a8b0230eb5fb6a4f987
spacessoradm <spacessor.ad@gmail.com>
2025-04-12 08:58:49 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -107,17 +107,7 @@ export default function Home() {
         <div className="container flex h-16 items-center justify-between">
           <div className="flex items-center gap-6 md:gap-10">
             <Link href="/" className="flex items-center space-x-2">
-              <svg viewBox="0 0 50 50" fill="white" className="h-8 w-8">
-                <path
-                  d="M25,2C12.3,2,2,12.3,2,25s10.3,23,23,23s23-10.3,23-23S37.7,2,25,2z M25,11c4.4,0,8,3.6,8,8s-3.6,8-8,8s-8-3.6-8-8
-                S20.6,11,25,11z M25,43c-5.3,0-10.2-2.1-13.8-5.5c-0.6-0.6-0.9-1.3-0.9-2.1c0-4.1,3.3-7.4,7.4-7.4h14.7c4.1,0,7.4,3.3,7.4,7.4
-                c0,0.8-0.3,1.5-0.9,2.1C35.2,40.9,30.3,43,25,43z"
-                />
-              </svg>
-              <div>
-                <div className="text-xl font-bold">Demo_Loan</div>
-                <div className="text-xs">Check | Apply | Approve</div>
-              </div>
+              <img src="https://pbvigsmmzasgbuqgwwdt.supabase.co/storage/v1/object/public/home//speedx_logo.png" style={{width: '150px'}} />
             </Link>
 
             <div className="hidden md:flex">
